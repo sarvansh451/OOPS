dbe1a5f3996bdff5bc4683a286bd6f8f96683f0c
sarvansh mehta <124300944+sarvansh451@users.noreply.github.com>
2024-10-17 10:51:30 +0530

    Add files via upload

---
+++ Weekday1.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Weekday1{
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the day number");
+        int dayNumb = scanner.nextInt();
+
+
+        // Determine and display the weekday using switch case
+        switch (dayNumb) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid day number");
+        }
+        scanner.close();
+    }
+}
